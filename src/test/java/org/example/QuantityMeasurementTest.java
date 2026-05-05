a0ea93f4070151d013a4fd816c6f367adc947309
AnshikaAsija2408 <155942951+AnshikaAsija2408@users.noreply.github.com>
2026-05-05 11:36:31 +0530

    Added UC2: Feet and inches measurement equality

--- src/test/java/org/example/QuantityMeasurementTest.java
+++ src/test/java/org/example/QuantityMeasurementTest.java
@@ -4,12 +4,13 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.example.QuantityMeasurement.Feet;
+import org.example.QuantityMeasurement.Inches;
 
 public class QuantityMeasurementTest {
 
     @Test
     public void testSameValue() {
-        Feet f1 = new Feet(1.0);
+        Feet f1 = new Feet(1.0);    //f1 is not the object. it si the reference
         Feet f2 = new Feet(1.0);
 
         assertTrue(f1.equals(f2));
@@ -17,15 +18,15 @@ public class QuantityMeasurementTest {
 
     @Test
     public void testDifferentValue() {
-        QuantityMeasurement.Feet f1 = new Feet(1.0);
-        QuantityMeasurement.Feet f2 = new Feet(2.0);
+        Feet f1 = new Feet(1.0);
+        Feet f2 = new Feet(2.0);
 
         assertFalse(f1.equals(f2));
     }
 
     @Test
     public void testNull() {
-        Feet f1 = new QuantityMeasurement.Feet(1.0);
+        Feet f1 = new Feet(1.0);
 
         assertFalse(f1.equals(null));
     }
@@ -42,5 +43,38 @@ public class QuantityMeasurementTest {
         Feet f1 = new Feet(1.0);
         String str = "1.0";
         assertFalse(f1.equals(str));
+    }
+
+    @Test
+    public void testInchesSameValue() {
+        Inches i1 = new Inches(1.0);
+        Inches i2 = new Inches(1.0);
+        assertTrue(i1.equals(i2));
+    }
+
+    @Test
+    public void testInchesDifferentValue() {
+        Inches i1 = new Inches(1.0);
+        Inches i2 = new Inches(2.0);
+        assertFalse(i1.equals(i2));
+    }
+
+    @Test
+    public void testInchesNull() {
+        Inches i1 = new Inches(1.0);
+        assertFalse(i1.equals(null));
+    }
+
+    @Test
+    public void testInchesDifferentClass() {
+        Inches i1 = new Inches(1.0);
+        Feet f1 = new Feet(1.0);
+        assertFalse(i1.equals(f1));
+    }
+
+    @Test
+    public void testInchesSameReference() {
+        Inches i1 = new Inches(1.0);
+        assertTrue(i1.equals(i1));
     }
 }
